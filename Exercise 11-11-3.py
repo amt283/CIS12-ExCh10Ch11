60e760e205d7a239bac029bf8b071576e19104f9
amt954 <athompson283@gmail.com>
2025-03-31 00:39:03 -0700

    Update for Ch 11.11.3

--- Exercise 11-11-3.py
+++ Exercise 11-11-3.py
@@ -7,7 +7,23 @@ Hints: Use the modulus operator to wrap around from 'z' back to 'a'. Loop throug
 one, and append the result to a list of letters. Then use join to concatenate the letters into a string."""
 
 def main():
-    pass
+    print(shift_word('cheer', 7)) # jolly
+    print(shift_word('melon', 16)) # cubed
+
+def shift_word(str_var, shift_int):
+    # Create a dictionary that maps from each letter to its index in the alphabet.
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    numbers = range(len(letters))
+    letter_map = dict(zip(letters, numbers))
+
+    caesar_word = []
+
+    # Loop through string and shift the letter based on the shift_int
+    # % 26 used to loop back to 'a' when 'z' is reached
+    for letter in str_var:
+        new_index = (letter_map[letter] + shift_int) % 26
+        caesar_word.append(letters[new_index])
+    return ''.join(caesar_word)
 
 if __name__ == "__main__":
     main()
