b3fc9a15de48f4a24b6dff09bf4dba831b8a3450
amt954 <athompson283@gmail.com>
2025-03-31 00:20:45 -0700

    Update for Ch 11.11.6

--- Exercise 11-11-6.py
+++ Exercise 11-11-6.py
@@ -4,7 +4,22 @@ where the two words differ.
 Hint: Use zip to loop through the corresponding letters of the words."""
 
 def main():
-    pass
+    print(word_distance("final", "finally"))  # Error
+    print(word_distance("grate", "plate"))  # 2
+
+def word_distance(word1, word2):
+    count = 0
+
+    # Check that words are the same length & use zip to pair letters from both words
+    # then count where the letters differ
+    if len(word1) == len(word2):
+        for first, second in zip(word1, word2):
+            if first != second:
+                count += 1
+    else:
+        return f"\"{word1.capitalize()}\" and \"{word2.capitalize()}\" are not the same length!"
+
+    return count
 
 if __name__ == "__main__":
     main()
