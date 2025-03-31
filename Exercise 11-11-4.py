11f443110bee9ea870b07ad7a1560d4f5eda50c7
amt954 <athompson283@gmail.com>
2025-03-30 23:54:40 -0700

    Update for Ch 11.11.4

--- Exercise 11-11-4.py
+++ Exercise 11-11-4.py
@@ -5,7 +5,20 @@ To get the items in decreasing order, you can use reversed along with sorted or 
 parameter to sorted."""
 
 def main():
-    pass
+    print(most_frequent_letters('mississippi'))
+
+def value_counts(string):
+    counter = {}
+    for letter in string:
+        if letter not in counter:
+            counter[letter] = 1
+        else:
+            counter[letter] += 1
+    return counter
+
+def most_frequent_letters(str_var):
+    counts = value_counts(str_var)
+    return list(reversed(sorted(counts)))
 
 if __name__ == "__main__":
     main()
